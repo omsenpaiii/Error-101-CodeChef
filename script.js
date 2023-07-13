102a6738b2c2263569964ffcf98eacd1889c6f51
omsenpaiii <123073338+omsenpaiii@users.noreply.github.com>
2023-07-14 01:12:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// script.js
+
+function navigateToHome() {
+    console.log("Navigating to Home");
+    // Logic to navigate to the Home page
+    window.location.href = "index.html";
+  }
+  
+  function navigateToAbout() {
+    console.log("Navigating to About");
+    // Logic to navigate to the About page
+    window.location.href = "templates/about.html";
+  }
+  
+  function navigateToInfo() {
+    console.log("Navigating to Info");
+    // Logic to navigate to the Info page
+    window.location.href = "templates/info.html";
+  }
+  
+  function navigateToServices() {
+    console.log("Navigating to Services");
+    // Logic to navigate to the Services page
+    window.location.href = "templates/services.html";
+  }
+  
+  function navigateToContact() {
+    console.log("Navigating to Contact");
+    // Logic to navigate to the Contact page
+    window.location.href = "templates/contact.html";
+  }
+  
