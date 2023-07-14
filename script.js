5409f2a8ae641aa6e153f5fad0e88b7c65ad5457
omsenpaiii <omtomar4882.be22@chitkara.edu.in>
2023-07-14 16:05:09 +0530

    New update for Functions

--- script.js
+++ script.js
@@ -9,24 +9,24 @@ function navigateToHome() {
   function navigateToAbout() {
     console.log("Navigating to About");
     // Logic to navigate to the About page
-    window.location.href = "templates/about.html";
+    window.location.href = "Templates/about.html";
   }
   
   function navigateToInfo() {
     console.log("Navigating to Info");
     // Logic to navigate to the Info page
-    window.location.href = "templates/info.html";
+    window.location.href = "Templates/info.html";
   }
   
   function navigateToServices() {
     console.log("Navigating to Services");
     // Logic to navigate to the Services page
-    window.location.href = "templates/services.html";
+    window.location.href = "Templates/services.html";
   }
   
   function navigateToContact() {
     console.log("Navigating to Contact");
     // Logic to navigate to the Contact page
-    window.location.href = "templates/contact.html";
+    window.location.href = "Templates/contact.html";
   }
   
